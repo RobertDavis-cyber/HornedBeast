dbe87068f2419dc33056e7fc8d9901680b2c2d8e
Robert Davis <robertcd@hawaii.edu>
2022-11-29 14:51:51 -1000

    lab1

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return <h1>HornedBeast 301Lab1</h1>   
+    }
+}
+
+export default Header;
---
+++ src/components/main.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Main extends React.Component {
+    render() {
+        return<div>
+       
+        <h2>This is my title</h2>
+                <img src="https://cdn.pixabay.com/photo/2016/10/26/23/19/horned-basket-1772958__340.jpg" 
+                alt='fishhorn' 
+                />
+             
+              <p>Description: A page for horned fish</p>
+                
+            </div>
+            }
+}
+
+export default Main;
